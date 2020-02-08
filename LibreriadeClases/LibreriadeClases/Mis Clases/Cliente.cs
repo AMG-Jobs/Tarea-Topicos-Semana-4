afa58fc8c1eb0b2cc700a91cc5e66c004701f755
AMG-jobs <andral33@yahoo.,es>
2020-02-08 15:51:33 -0600

    Parte 1 Modelo NorthWind

---
+++ LibreriadeClases/LibreriadeClases/Mis Clases/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriadeClases
+{
+    public partial class Customers
+    {
+        public string Direccion {
+            get {
+                //pais,ciudad,direccion,codpostal
+                var direccion = string.Format("{0}, {1}, {2}, {3}", Country, City, Address, PostalCode);
+                return direccion;
+            }
+            set { }
+        }
+
+        public string InformacionContacto {
+            get {
+                //nombre, title,company
+                var informacionContacto = string.Format("{0}, {1} of {2}", ContactName, ContactTitle, CompanyName);
+                return informacionContacto;
+            }
+            set { }
+        }
+    }
+}
---
+++ LibreriadeClases/LibreriadeClases/Mis Clases/Orders.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriadeClases
+{
+    public partial class Orders
+    {
+        public string DirecionEntrega {
+            get
+            {
+                //pais,ciudad, direccion,postal
+                if (ShipPostalCode != null) {
+                    var direccionEntrega = string.Format("{0}, {1}, {2}, {3}", ShipCountry, ShipCity, ShipAddress, ShipPostalCode);
+                    return direccionEntrega;
+                }
+                else
+                {
+                    var direccionEntrega = string.Format("{0}, {1}, {2}", ShipCountry, ShipCity, ShipAddress);
+                    return direccionEntrega;
+                }
+                
+            }
+            set { }
+        }
+    }
+}
