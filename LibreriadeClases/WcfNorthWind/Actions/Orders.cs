8a956f143978eff0d0eb12e7e00b1e9957d5b026
Laboratorio <Laboratorio@SP-LA-LAB9-08>
2020-02-15 16:07:33 -0600

    Se agrega el LInQ

---
+++ LibreriadeClases/WcfNorthWind/Actions/Orders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibreriadeClases;
+
+namespace WcfNorthWind.Actions
+{
+    internal class Orders
+    {
+        private static NorthwindEntities _elRepositorio = new NorthwindEntities();
+
+        public IList<LibreriadeClases.Orders> OrdersByShipper(int shipperID)
+        {
+            var elRepositorio = new Repositorios.Orders();
+            var elResultado = elRepositorio.OrdersByShipper(shipperID);
+            return elResultado;
+        }
+    }
+}
---
+++ LibreriadeClases/WcfNorthWind/Repositorios/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibreriadeClases;
+
+namespace WcfNorthWind.Repositorios
+{
+    internal class Orders
+    {
+        private static NorthwindEntities _elRepositorio = new NorthwindEntities();
+
+        internal IList<LibreriadeClases.Orders> OrdersByShipper(int shipperID)
+        {
+            var lasOrdenes = _elRepositorio.Orders.Where(s => s.ShipVia.Equals(shipperID)).ToList();
+            return lasOrdenes;
+        }
+    }
+}
